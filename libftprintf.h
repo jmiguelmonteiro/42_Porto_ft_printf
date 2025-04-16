3682a3de69a318918f09c046ff5cddaab4007617
Miguel <miguel@MacBook-Pro-de-Miguel.local>
2025-04-16 13:42:59 +0100

    20251604.1342

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 13:35:50 by josemigu          #+#    #+#             */
+/*   Updated: 2025/04/16 13:40:51 by josemigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBLIBFTPRINTF_HFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
