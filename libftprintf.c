d78c1521f980d9c05850cd09953bb00de2c18260
josemigu <josemigu@student.42porto.com>
2025-04-19 19:05:00 +0100

    20250419.1904

--- libftprintf.c
+++ libftprintf.c
@@ -6,7 +6,7 @@
 /*   By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 13:36:08 by josemigu          #+#    #+#             */
-/*   Updated: 2025/04/16 19:19:45 by josemigu         ###   ########.fr       */
+/*   Updated: 2025/04/19 18:01:40 by josemigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,21 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (*format)
 	{
-		if (*format == '%' && *(format + 1) == 's')
+		if (*format == '%' && *(format + 1) == '%')
+		{
+			write(1, '%', 1);
+			format += 2;
+			nbytes++;
+			break;
+		}
+/* 		if (*format == '%' && *(format + 1) == 's')
 		{
 			printf("%s", "Tipo s");
-			format++;
+			format += 2;
+			break;
 		}
-		else
-		{
-			write(1, format, 1);
-			nbytes++;
-		}
-		format++;
+ */		write(1, format, 1);
+		nbytes++;
 	}
 	return (NULL);
 }
